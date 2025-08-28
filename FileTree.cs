7fc8148b662f0b44c1d60dd57d583ccf507d3db6
YUBIZ <k.sein1016@gmail.com>
2025-08-29 05:02:14 +0900

    EnumerateFiles가 루트 트리의 이름을 앞에 붙이도록 변경

--- FileTree.cs
+++ FileTree.cs
@@ -2,14 +2,14 @@ public readonly record struct FileTree(string Name, FileTree[] SubTrees, string[
 {
     public IEnumerable<string> EnumerateFiles()
     {
-        return EnumerateFilesInternal("");
+        return EnumerateFilesInternal(Name);
     }
 
     private IEnumerable<string> EnumerateFilesInternal(string basePath)
     {
         foreach (var subTree in SubTrees)
         {
-            foreach (var file in subTree.EnumerateFilesInternal( Path.Combine(basePath, subTree.Name))) yield return file;
+            foreach (var file in subTree.EnumerateFilesInternal(Path.Combine(basePath, subTree.Name))) yield return file;
         }
 
         foreach (var file in Files) yield return Path.Combine(basePath, file);
